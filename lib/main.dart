9b223c5c9db12d50bca21f476adb05cab3dc5000
SamandarTakhirov <itbloog741@gmail.com>
2023-06-05 09:27:54 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -11,175 +11,86 @@ void main() {
             child: Container(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Color(0xFF626262), Color(0xFFC5C5C5)],
+                  colors: [Color(0xFFD5D5D5),
+                    Color(0xFFF1F1F1)],
                   begin: Alignment.topRight,
                   end: Alignment.bottomLeft,
                 ),
               ),
               child: Expanded(
-                child: Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(top: 15, left: 140, right: 100),
-                        child: Row(
-                          children: [
-                            Container(
-                              child: Text(
-                                "#364649",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 150, left: 50, right: 200),
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 20),
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFE21C34),
+                              boxShadow:[BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 10,
+                              )],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(left: 20),
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xFF364649),
-                                boxShadow:[BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 10,
-                                )],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 120, right: 120, top: 15),
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            end: Alignment.bottomLeft,
-                            colors: [Color(0xFF364649), Color(0xFF708F96)],
                           ),
-                          boxShadow:[BoxShadow(
-                            color: Colors.black,
-                            blurRadius: 10,
-                          )],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 15, left: 120, right: 130),
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xFF708F96),
-                                boxShadow:[BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 10,
-                                )],
-                              ),
+                          Container(
+                            child: Text(
+                              "#E21C34",
+                              style: TextStyle(
+                                  color: Colors.grey, fontSize: 20),
                             ),
-                            Container(
-                              child: Text(
-                                "#708F96",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 120, right: 120, top: 15),
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [Color(0xFF708F96), Color(0xFFAA895F)],
                           ),
-                          boxShadow:[BoxShadow(
-                            color: Colors.black,
-                            blurRadius: 10,
-                          )],
-                        ),
+                        ],
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: 15, left: 140, right: 100),
-                        child: Row(
-                          children: [
-                            Container(
-                              child: Text(
-                                "#AA895F",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 20),
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xFFAA895F),
-                                boxShadow:[BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 10,
-                                )],
-                              ),
-                            ),
-                          ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(50),
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Color(0xFFE21C34), Color(0xFF500B28)],
                         ),
+                        boxShadow:[BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                        )],
                       ),
-                      Container(
-                        margin: EdgeInsets.only(left: 120, right: 120, top: 15),
-                        height: 150,
-                        decoration: BoxDecoration(
-                          boxShadow:[BoxShadow(
-                            color: Colors.black,
-                            blurRadius: 10,
-                          )],
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            end: Alignment.bottomLeft,
-                            colors: [Color(0xFFAA895F), Color(0xFFE0D8CC)],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left:200,right: 50),
+                      child: Row(
+                        children: [
+                          Container(
+                            child: Text(
+                              "#500B28",
+                              style: TextStyle(
+                                  color: Colors.grey, fontSize: 20),
+                            ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 15, left: 120, right: 130),
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color(0xFFE0D8CC),
-                                  boxShadow:[BoxShadow(
-                                    color: Colors.black,
-                                    blurRadius: 10,
-                                  )],
-                              ),
+                          Container(
+                            margin: EdgeInsets.only(left: 20),
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFF500B28),
+                              boxShadow:[BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 10,
+                              )],
                             ),
-                            Container(
-                              child: Text(
-                                "#E0D8CC",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
